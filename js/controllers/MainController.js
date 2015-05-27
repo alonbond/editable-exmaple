32cb46d616b59ebab17c40b2ffbfe8372f804d59
Alon Weissfeld <alonbond@gmail.com>
2015-05-27 17:23:15 +0300

    Multi medicines/families

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,3 +1,20 @@
-app.controller('MainController', ['$scope', function($scope){
-      
+app.controller('MainController', ['$scope', '$templateCache', function($scope, $templateCache){
+	// $scope.outcomes = [];
+	$scope.medicines = [{
+		name: 'Example 0'
+	}];
+	var counter = 1; 
+
+	$scope.addMedicine = function(){
+		var medicine = {
+			name: 'Example ' + counter
+		};
+		$scope.medicines.push(medicine);
+
+		counter += 1;
+	}
+
+	$scope.removeMedicine = function(){
+		$scope.medicines.pop();
+	}
 }]);
